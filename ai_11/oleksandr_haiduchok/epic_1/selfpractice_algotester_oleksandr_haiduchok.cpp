3187937f7e6dec29b8c840d7fc0d31c12e29ac8a
Alex-H11 <sashagaidychok@gmail.com>
2026-09-16 02:00:08 +0300

    Create selfpractice_algotester_oleksandr_haiduchok.cpp
    Added a new C++ file for self-practice tasks.

---
+++ ai_11/oleksandr_haiduchok/epic_1/selfpractice_algotester_oleksandr_haiduchok.cpp
@@ -0,0 +1,16 @@
+/*Epic1_Oleksandr_Haiduchok_ShI-11
+  Задача А + Б*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a,b;
+    cin >> a;
+    cin >> b;
+    cout << a+b;
+    return 0;
+}
