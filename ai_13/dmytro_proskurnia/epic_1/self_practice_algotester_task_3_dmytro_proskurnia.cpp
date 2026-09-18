8b1b9088b4f60f08ae968a6d24f3808ab06c4e21
Proskurnia Dmytro <ravlikgmgailcom@gmail.com>
2026-09-18 09:50:50 +0000

    change self_practice_task

---
+++ ai_13/dmytro_proskurnia/epic_1/self_practice_algotester_task_3_dmytro_proskurnia.cpp
@@ -0,0 +1,18 @@
+// 0921
+#include <iostream>
+
+using namespace std;
+
+signed main()
+{
+    int a, b; cin >> a >> b;
+    if (a > b)
+        swap(a, b);
+    if (a + 1 >= b) {
+        cout << -1;
+    } else {
+        cout << a + 1;
+    }
+
+    return 0;
+}
