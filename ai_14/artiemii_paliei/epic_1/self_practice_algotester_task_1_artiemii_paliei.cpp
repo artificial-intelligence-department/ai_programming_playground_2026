8fa32b9ed8e2ed71231dcaddddcb0981a3a8f903
Artiemii Paliei <artiemii.paliei.shi.2026@lpnu.ua>
2026-09-20 20:44:54 +0000

    Epic 1 - Artiemii Paliei

--- ai_14/artiemii_paliei/epic_1/self_practice_algotester_task_1_artiemii_paliei.cpp
+++ ai_14/artiemii_paliei/epic_1/self_practice_algotester_task_1_artiemii_paliei.cpp
@@ -2,7 +2,7 @@
 int main()
 {
     int a, b;
-    if (a<0 || b<0 or a>100 or b>100)
+    if (a<0 || b<0 || a>100 || b>100)
         return 1;
     std::cin >> a >> b;
     std::cout << a + b << std::endl;
