4e79121e3ff8b4d0c2b39648d6e553b3698aa84a
Vadym Rudyka <vadymrudyka2009@gmail.com>
2026-09-20 19:54:47 +0000

    Epic 1 - Vadym Rudyka

---
+++ ai_12/vadym_rudyka/epic_1/self_practice_algotester_task_1_vadym_rudyka.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Введіть число 1: ";
+    cin >> a;
+    cout << "Введіть число 2: ";
+    cin >> b;
+    cout << "Сума: " << a+b;
+    return 0;
+} 
