1728bad209b5c258ec606238a9e0d2bb6bceffbd
Віталій Карпінський <vitalik922009@gmail.com>
2026-09-20 12:48:15 +0000

    виконав додаткову задачу з алготестера 0011

---
+++ ai_14/vitalii_karpinskyi/epic_1/self_practice_algotester_task_0011_vitalii_karpinskyi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    long long total = 0;
+    for (int i = 1; i <= n; i = i + 1) {
+        long long a;
+        cin >> a;
+        total = total + (a-1);
+    }
+    cout << total << endl;
+
+    return 0;
+}
