5971ec59ab55bb52e3e763294d2d43379ddd45ae
andriimadiarshi2026-a11y <andrii.madiar.shi.2026@lpnu.ua>
2026-09-21 17:30:49 +0000

    add epic 1 code

---
+++ ai_13/andrii_maduar/epic_1/self_practice_algotester_task_1_andrii_maduar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=0, b=0;
+    int a0=0, b0=0;
+    cout << "Введіть число a:" << a0 << "Введіть число b:" << b0 << endl;
+    cin >> a0 >> b0;
+    if (a0<0 || a0>100){
+      cout << "Неправильне введення" << endl;
+     return 0;
+    }
+    else if (b0<0 || b0>100){
+      cout << "Неправильне введення" << endl;
+     return 0;
+    }
+    else a=a0, b=b0;
+    int S=a+b;
+    cout << S << endl;
+    return 0;
+}
