ed7fb210ab5fb2f1d922fb5bf7903152453473af
Daniel Onyshko <daniel.onyshko.shi.2026@lpnu.ua>
2026-09-21 15:17:04 +0300

    refactor: remove comments from C++ file

--- ai_14/onyshkodaniel/epic_1/practice_task_1_daniel_onyshko.cpp
+++ ai_14/onyshkodaniel/epic_1/practice_task_1_daniel_onyshko.cpp
@@ -26,23 +26,23 @@ int main() {
     cout << "Vvedit potuzhnist priladu (Vt): ";
     cin >> P;
 
-    // Це перевірка чи будуть помилки
+
     if (C <= 0 || years < 0 || charge < 0 || eff <= 0 || P <= 0) {
         cout << "Error: Nekorektni dani!" << endl;
         return 0;
     }
 
-    // Розрахунки
-    double C_eff = C * (1.0 - 0.02 * years);      // Ємність з урахуванням віку
-    double E_stored = C_eff * (charge / 100.0);    // Запас енергії
-    double E_useful = E_stored * (eff / 100.0);    // Корисна енергія
-    double E_loss = E_stored - E_useful;          // Втрати
-    double T = E_useful / P;                      // Час у годинах
 
-    int hours = T;                                // Цілі години
-    int minutes = (T - hours) * 60;               // Хвилини
+    double C_eff = C * (1.0 - 0.02 * years);      
+    double E_stored = C_eff * (charge / 100.0);   
+    double E_useful = E_stored * (eff / 100.0);   
+    double E_loss = E_stored - E_useful;         
+    double T = E_useful / P;                     
 
-    // Виводить результатию 
+    int hours = T;                                
+    int minutes = (T - hours) * 60;               
+
+    
     cout << "\n--- REZULTAT ---" << endl;
     cout << "Model: " << model << endl;
     cout << "Faktychna emnist: " << C_eff << " Vt*god" << endl;
