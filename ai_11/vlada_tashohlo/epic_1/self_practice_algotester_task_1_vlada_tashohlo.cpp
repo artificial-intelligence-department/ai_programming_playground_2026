9a33774d4849c255f115d0fa3f65f00c7c5a0035
Vlada Tashohlo <vlada.tashohlo.shi.2026@lpnu.ua>
2026-09-22 17:58:37 +0000

    Epic 1 - Vlada Tashohlo

---
+++ ai_11/vlada_tashohlo/epic_1/self_practice_algotester_task_1_vlada_tashohlo.cpp
@@ -0,0 +1,19 @@
+/* 
+Епік 1. self practice: Algotester, задача "A плюс B"
+Авторка: Влада Ташогло
+Група: ші-11
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    cout << num1 + num2 << endl;
+
+    return 0;
+
+}
