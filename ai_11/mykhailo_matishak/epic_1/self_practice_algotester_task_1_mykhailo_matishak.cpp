86e33ae78afe7ee674ceb12e8a18c3ba70b602bf
Mykhailo Matishak <acumenverse@gmail.com>
2026-09-24 18:06:09 +0000

    Epic 1 - Mykhailo Matishak

---
+++ ai_11/mykhailo_matishak/epic_1/self_practice_algotester_task_1_mykhailo_matishak.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (cin.fail())
+    {
+        cout << "Помилка: A і B мають бути цілими числами." << endl;
+        return 1;
+    }
+
+    if (a < 0 || a > 100 || b < 0 || b > 100)
+    {
+        cout << "Помилка: A і B мають бути в межах від 0 до 100." << endl;
+        return 1;
+    }
+
+    cout << a + b << endl;
+    return 0;
+}
