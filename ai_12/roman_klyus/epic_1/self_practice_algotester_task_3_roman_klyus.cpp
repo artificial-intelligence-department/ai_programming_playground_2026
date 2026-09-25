53371ff01bdc487ce5b014193c4952158ce42def
Роман Клюс <mainwreaxx18@gmail.com>
2026-09-25 07:38:44 +0000

    renamed

--- ai_12/roman_klyus/epic_1/self_practice_algotester_task_3_roman_klyus.cpp
+++ ai_12/roman_klyus/epic_1/self_practice_algotester_task_3_roman_klyus.cpp
@@ -7,7 +7,17 @@ int main() {
     int a, b, c, r;
     cin >> a >> b >> c >> r;
 
-    
+    double p = (a + b + c) / 2.0;
+    double S = pow(p*(p - a)*(p - b)*(p - c), 0.5);
+    double r1 = S / p;
+    double R1 = (a * b * c) / (4 * S);
+
+    if (R1 <= r) cout << '+';
+    else cout << '-';
+
+    if (r1 >= r) cout << "+";
+    else cout << '-' << endl;
+
 
     return 0;
 }
